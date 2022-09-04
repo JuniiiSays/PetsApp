7b8784e82e1fa27a0adbceb2a2e78442dbfcd369
Junaid Iqbal <Junaid.jigar761@gmail.com>
2022-09-04 13:19:30 +0500

    Gender constant is used in EditorActivity.java

--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -4,7 +4,7 @@ import android.provider.BaseColumns;
 
 public final class PetContract {
 
-    public static abstract class PetEntry implements BaseColumns {
+    public static final class PetEntry implements BaseColumns {
         public static final String TABLE_NAME = "pets";
         public static final String _ID = BaseColumns._ID;
         public static final String COLUMN_PET_NAME = "name";
